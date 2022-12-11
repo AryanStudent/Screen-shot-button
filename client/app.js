abe2d06442312a1ce39813b3c60b40885b5a63ef
AryanStudent <119576342+AryanStudent@users.noreply.github.com>
2022-12-11 18:36:53 +0530

    Update app.js

--- client/app.js
+++ client/app.js
@@ -1,61 +1,56 @@
 const { app, BrowserWindow, ipcMain } = require('electron')
-const {v4 : uuidv4 } =require('uuid');
-const screenshare = require('screenshot-desktop');
-const path = require('path')
+const { v4: uuidv4 } = require('uuid');
+const screenshot = require('screenshot-desktop');
 
-var socket = require('socket.io-client')('117.99.191.98:5000')
+var socket = require('socket.io-client')('http://192.168.0.100:5000');
 var interval;
 
 function createWindow () {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
-  })
-
-  win.loadFile('index.html')
-  win.removeMenu();
+    const win = new BrowserWindow({
+        width: 500,
+        height: 150,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
+    win.removeMenu();
+    win.loadFile('index.html')
 }
 
-app.whenReady().then(() => {
-  createWindow()
-
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
-    }
-  })
-})
+app.whenReady().then(createWindow)
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
 })
 
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+    }
+})
 
 ipcMain.on("start-share", function(event, arg) {
 
-  var uuid = uuidv4();
-  socket.emit("join-message", uuid);
-  event.reply("uuid", uuid);
+    var uuid = uuidv4();
+    socket.emit("join-message", uuid);
+    event.reply("uuid", uuid);
 
-  interval = setInterval(function() {
-      screenshot().then((img) => {
-          var imgStr = new Buffer(img).toString('base64');
+    interval = setInterval(function() {
+        screenshot().then((img) => {
+            var imgStr = new Buffer(img).toString('base64');
 
-          var obj = {};
-          obj.room = uuid;
-          obj.image = imgStr;
+            var obj = {};
+            obj.room = uuid;
+            obj.image = imgStr;
 
-          socket.emit("screen-data", JSON.stringify(obj));
-      })
-  }, 100)
+            socket.emit("screen-data", JSON.stringify(obj));
+        })
+    }, 100)
 })
 
 ipcMain.on("stop-share", function(event, arg) {
 
-  clearInterval(interval);
+    clearInterval(interval);
 })
